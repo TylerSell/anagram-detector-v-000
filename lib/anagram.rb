06bc0e8c125cf9f42fa57679b9a4d178273cfd90
tylerselldesign@gmail.com <tylerselldesign@gmail.com>
2018-09-08 04:53:27 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word 
+  
+  @@all = []
+  
+  def initialize(word)
+    @word = word 
+  end
+  
+  def match(array)
+    array.map do |anagrams|
+      if anagrams.split("").sort == @word.split("").sort 
+        @@all << anagrams
+      end
+    end
+  end
+  
+end
